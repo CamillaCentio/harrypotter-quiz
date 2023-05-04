6e0b82d021907ae4f5d01eb8b75854a1b06b8d06
Camilla Centio <camillacentio@gmail.com>
2023-05-04 12:29:24 +0000

    Adding code from local

--- README.md
+++ README.md
@@ -1,31 +1,228 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+<div align="center">
+  <a href="#" target="_blank">
+    <img src="assets/images/logo.jpg" alt="Logo" width="250" height="100">
+  </a>
 
-Welcome USER_NAME,
+  <h3 align="center">Project 3 - Harry Potter Quiz</h3>
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+  <p align="center">
+    My Python project for Code Institute. Harry Potter Quiz.
+    <br />
+    <br />
+    <a href="#" target="_blank">View Demo Website</a>
+  </p>
+</div>
 
-## Reminders
+## Table of Contents
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li><a href="#Features">Features</a></li>
+    <li><a href="#How-to-Play">How to Play</a></li>
+    <li><a href="#Game-Instructions">Game Instructions</a></li>
+    <li><a href="#Scoring">Scoring</a></li>
+    <li><a href="#Known-Bugs-and-Fixes">Known Bugs and Fixes</a></li>
+    <li><a href="#Data-Model">Data Model</a></li>
+    <li><a href="#Validating-&-Testing">Validating & Testing</a></li>
+    <li><a href="#Credits">Credits</a></li>
+  </ol>
+</details>
+<br />
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
 
-## Creating the Heroku app
+# Python Harry Potter Quiz game 🧐
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+Test your knowledge of the Harry Potter books and movies with this fun quiz game! Answer 10 questions in each of four categories: General Knowledge, Hogwarts Houses, Magical Creatures, and Spells. Each correct answer is worth 10 points, and your score will be added to the scoreboard.
 
-1. `heroku/python`
-2. `heroku/nodejs`
+![Mockup of the game](assets/images/mockup.png)
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+# Features
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+- Four categories of Harry Potter quiz questions
+- Google Sheets integration for scoreboard tracking
+- User authentication for name and age
+- User input validation for name and age
 
-Connect your GitHub repository and deploy as normal.
+  ![Features of the game](assets/images/features4.png)
 
-## Constraints
+---
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+## How to Play
 
------
-Happy coding!
+1. Clone this repository to your local machine.
+2. Install the necessary dependencies using: `pip install -r requirements.txt`.
+3. Obtain a creds.json file for authentication with the Google Sheets API.
+4. Run `python quiz.py` to start the game.
+5. Choose a category (1-4) and answer 10 questions to test your Harry Potter knowledge!
+6. After completing a quiz, you will be prompted to play again or quit.
+7. At any time, you can view the top five scores on the scoreboard.
+
+![Fun Math Game Screenshot 2](assets/images/howtoplay.png)
+
+---
+
+## Game Instructions
+
+1. Choose a category by entering a number from 1 to 4 when prompted.
+2. For each question, enter your answer and press Enter.
+3. Your answer will be compared to the correct answer for that question.
+4. If your answer is correct, you will receive 10 points.
+5. If your answer is incorrect, the correct answer will be displayed and you will receive no points.
+6. After 10 questions, your score will be displayed.
+7. Your score will be added to the scoreboard, and the top five scores will be displayed.
+
+---
+
+## Scoring
+
+- Each correct answer is worth 10 points.
+- A perfect score is 100 points.
+- Your score will be added to the scoreboard, and the top five scores will be displayed.
+
+![Scoreboard of the game](assets/images/scores.png)
+
+---
+
+## Known Bugs and Fixes
+
+* Bug reading from google sheet to the game **(*Solved*)**
+* Error when choosing question answers (= categories[str(int(category) + 1)]["answers"]) **(*Solved*)**
+* There are no more known bugs at this time.
+
+---
+
+## Data Model
+
+**categories (dict)**
+
+A dictionary containing four categories of Harry Potter quiz questions. Each category has a unique key, with the following properties:
+
+- name (str): the name of the category
+- questions (list of str): a list of ten quiz questions for the category
+- answers (list of str): a list of the correct answers for the questions in the category
+
+
+**get_player_info() (function)**
+
+A function that prompts the user for their name and age, and returns them as a tuple.
+
+
+**play_game(category) (function)**
+
+A function that takes a category as input and plays a game of Harry Potter quiz based on the questions in that category. The function shuffles the questions, prompts the user for answers, and returns the user's score out of 100.
+
+
+**scoreboard (list of dict)**
+
+A list of dictionaries representing the top five scores on the Harry Potter quiz. Each dictionary has the following properties:
+
+- Name (str): the name of the player
+- Age (int): the age of the player
+- Score (int): the player's score on the quiz
+
+**creds.json (file)**
+
+A JSON file containing the credentials for accessing the Google Sheets API.
+
+
+**scope (list of str)**
+
+A list of two strings representing the scopes for the Google Sheets API: "https://spreadsheets.google.com/feeds" and "https://www.googleapis.com/auth/drive".
+
+
+**client (Google Sheets API client)**
+
+A client for accessing the Google Sheets API, authenticated using the credentials in creds.json and the scope in scope.
+
+
+**sheet_name (str)**
+
+The name of the Google Sheets worksheet where the scoreboard data is stored.
+
+
+**sheet (Google Sheets worksheet)**
+
+The worksheet in the Google Sheets API where the scoreboard data is stored.
+
+**header_row (list of str)**
+
+A list of strings representing the headers of the columns in the scoreboard worksheet.
+
+
+**scoreboard (list of dict)**
+
+A list of dictionaries representing the top five scores on the Harry Potter quiz, read from the Google Sheets worksheet. Each dictionary has the following properties:
+
+- Name (str): the name of the player
+- Age (int): the age of the player
+- Score (int): the player's score on the quiz
+
+**scoreboard.sort() (function)**
+
+A function that sorts the scoreboard in descending order by score.
+
+**print() (function)**
+
+A function that displays the top five scores on the Harry Potter quiz, as well as prompts for user input and displays quiz information and results.
+
+---
+
+## Validating & Testing
+
+- Validate the code from Code Institutes [CI Pyhton Linter](https://pep8ci.herokuapp.com/) with error for trailing whitespace for the ascii art.
+
+  ![Screenshot from CI Python Linter](assets/images/testing.png)
+
+- The game is tested in diffrent browsers, Chrome, Safari and Firefox, without problem.
+- The game is also tested in VScode desktop version
+- The game is reading and writing to Google Sheet without problems to Sheet1!A2:C.
+- This code has been tested on Python 3.8.10 and GSpread 4.0.1. 
+
+  ![Screenshot from Google Sheet](assets/images/googlesheet.png)
+
+---
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+---
+
+## Requirements
+
+- Python 3.x
+- GSpread
+
+---
+
+## Customization
+
+To customize the quiz, you can modify the categories dictionary in the code to add or remove categories and questions. 
+* You can also change the creds.json file to use your own Google Sheets credentials and sheet name. 
+* Finally, you can customize the messages and prompts displayed to the user in the code.
+
+----
+
+## Contributing
+
+If you would like to contribute to this project, please feel free to submit a pull request or open an issue on GitHub. I appreciate any suggestions or improvements to the game.
+
+---
+
+## Credits
+
+* This code was written by Camilla Centio and is licensed under the MIT License. The Harry Potter quiz questions were sourced from the movies and books about Harry Potter. 
+* The Google Sheets integration was built using the GSpread library.
+
+---
+
+## Author
+
+- Camilla Centio - _Initial work_ - [CamillaCentio](https://github.com/camillacentio)
+
+---
+
+## Contact
+
+If you have any questions or suggestions, feel free to reach out to me at camillacentio@gmail.com
--- run.py
+++ run.py
@@ -1,3 +1,225 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import re
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Define the questions and answers
+categories = {
+    "1": {"name": "General Knowledge", "questions": [
+        "What is the name of Harry Potter's owl?",
+        "What is the name of the Hogwarts gamekeeper?",
+        "What is the name of the magical creature that can transform into a person's worst fear?",
+        "Who killed Dumbledore?",
+        "What is the name of the Weasley family's pet rat?",
+        "What is the name of the ghost who haunts the girls' bathroom?",
+        "What is the name of the werewolf who teaches Defense Against the Dark Arts in Harry's third year?",
+        "What is the name of the house elf who serves the Malfoy family?",
+        "What is the name of Harry's godfather?",
+        "What is the name of the wizarding prison where Sirius Black was held?"
+    ], "answers": [
+        "Hedwig",
+        "Hagrid",
+        "Boggart",
+        "Severus Snape",
+        "Scabbers",
+        "Moaning Myrtle",
+        "Remus Lupin",
+        "Dobby",
+        "Sirius Black",
+        "Azkaban"
+    ]},
+    "2": {"name": "Hogwarts Houses", "questions": [
+        "What is the animal mascot of Hufflepuff house?",
+        "What is the ghost mascot of Ravenclaw house?",
+        "What is the common room password for Gryffindor house in Harry's first year?",
+        "What is the founder of Slytherin house's first name?",
+        "What is the animal mascot of Ravenclaw house?",
+        "What is the animal mascot of Gryffindor house?",
+        "What is the ghost mascot of Hufflepuff house?",
+        "What is the founder of Hufflepuff house's first name?",
+        "What is the common room password for Slytherin house in Harry's second year?",
+        "What is the animal mascot of Slytherin house?"
+    ], "answers": [
+        "Badger",
+        "Grey Lady",
+        "Caput Draconis",
+        "Salazar",
+        "Eagle",
+        "Lion",
+        "Fat Friar",
+        "Helga",
+        "Pureblood",
+        "Serpent"
+    ]},
+    "3": {"name": "Magical Creatures", "questions": [
+        "What is the name of Hagrid's three-headed dog?",
+        "What is the name of the half-eagle, half-horse creature that serves as the Hogwarts gamekeeper?",
+        "What is the name of the magical creature that can transform into a person's worst fear?",
+        "What is the name of the giant spider that lives in the Forbidden Forest?",
+        "What is the name of the snake that Harry can speak to?",
+        "What is the name of the dragon that Hagrid hatches from an egg?",
+        "What is the name of the giant squid that lives in the Hogwarts lake?",
+        "What is the name of the ghost who haunts the Hogwarts castle and is also known as the Bloody Baron?",
+        "What is the name of the creature that is half-man, half-goat and is known for its love of wine and parties?",
+        "What is the name of the creature that is a small, furry ball with wings that can fly?"
+    ], "answers": [
+        "Fluffy",
+        "Fang",
+        "Boggart",
+        "Aragog",
+        "Nagini",
+        "Norbert",
+        "The Giant Squid",
+        "The Bloody Baron",
+        "The Satyr",
+        "The Golden Snidget"
+    ]},
+    "4": {"name": "Spells", "questions": [
+        "What spell does Harry use to disarm opponents?",
+        "What spell does Ron use to levitate objects?",
+        "What spell does Hermione use to create a Patronus?",
+        "What spell does Voldemort use to kill?",
+        "What spell does Snape use to prevent Harry from talking during his detention?",
+        "What spell does Harry use to summon his broomstick in his first year at Hogwarts?",
+        "What spell does Professor McGonagall use to bring the Hogwarts statues to life during the Battle of Hogwarts?",
+        "What spell does Hermione use to repair Harry's glasses in the Sorcerer's Stone?",
+        "What spell does Snape use to clear up Neville's potion in the Sorcerer's Stone?",
+        "What spell does Harry use to make his wand emit a light in the Sorcerer's Stone?"
+    ], "answers": [
+        "Expelliarmus",
+        "Wingardium Leviosa",
+        "Expecto Patronum",
+        "Avada Kedavra",
+        "Silencio",
+        "Accio",
+        "Piertotum Locomotor",
+        "Oculus Reparo",
+        "Wingardium Leviosa",
+        "Lumos"
+    ]}
+}
+
+
+# Function to get the player's name and age
+def get_player_info():
+    while True:
+        name = input("What is your name? ")
+        if not re.match("^[A-Za-z]*$", name):
+            print("Sorry, only letters are allowed in the name. Please try again.")
+        else:
+            break
+
+    while True:
+        age = input("What is your age? ")
+        if not age.isdigit() or int(age) < 1 or int(age) > 105:
+            print("Sorry, only numbers between 1 and 105 are allowed in the age. Please try again.")
+        else:
+            break
+
+    return name, int(age)
+
+
+# Function to play the game
+def play_game(category):
+    # Shuffle the questions for the chosen category
+    questions = categories[str(category)]["questions"]
+    answers = categories[str(category)]["answers"]
+    questions_and_answers = list(zip(questions, answers))
+    random.shuffle(questions_and_answers)
+
+    # Ask the questions and keep score
+    score = 0
+    for i, (question, answer) in enumerate(questions_and_answers[:10]):
+        print(f"\nQuestion {i+1}: {question}")
+        player_answer = input("Your answer: ")
+        if player_answer.lower() == answer.lower():
+            print("Correct!")
+            score += 10
+        else:
+            print(f"Sorry, the correct answer is {answer}.")
+
+    return score
+
+
+# Define the scoreboard
+scoreboard = []
+
+
+# Authenticate with the Google Sheets API
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = Credentials.from_service_account_file("creds.json", scopes=scope)
+client = gspread.authorize(creds)
+
+# Open the scoreboard sheet
+sheet_name = "Harry Potter Quiz Scoreboard"
+sheet = client.open(sheet_name).sheet1
+
+# Read the scoreboard data
+header_row = sheet.row_values(1)
+scoreboard = []
+for row in sheet.get_all_values()[1:]:
+    scoreboard.append(dict(zip(header_row, row)))
+
+# Sort the scoreboard by score
+scoreboard.sort(key=lambda x: x["Score"], reverse=True)
+
+# Display the scoreboard
+print("\nTop 5 Scores:")
+for i, row in enumerate(scoreboard[:5]):
+    print(f"{i+1}. {row['Name']} ({row['Age']}): {row['Score']}")
+
+# Main loop
+while True:
+    print("""
+ _  _    __    ___   ___  __   __   ___    __    _____   _____   ___   ___  
+| || |  /  \  | _ \ | _ \ \ `v' /  | _,\  /__\  |_   _| |_   _| | __| | _ \ 
+| >< | | /\ | | v / | v /  `. .'   | v_/ | \/ |   | |     | |   | _|  | v / 
+|_||_| |_||_| |_|_\ |_|_\   !_!    |_|    \__/    |_|     |_|   |___| |_|_\ 
+
+    """)
+    print("\nWelcome to the Harry Potter Quiz!")
+    print("Answer 10 questions to test your knowledge of the Harry Potter books and movies.")
+    print("Each correct answer is worth 10 points.")
+    print("\nYou can choose from the following categories:")
+    for category in categories:
+        print(f"{category}. {categories[category]['name']}")
+    category = input("Enter the category number you want to play (1-4): ")
+
+    if category in categories:
+        name, age = get_player_info()
+        print(f"\nWelcome, {name}!")
+        score = play_game(category)
+
+        print(f"\nThank you for playing, {name}!")
+        print(f"Your final score is {score} out of 100.")
+
+        # Update the scoreboard4
+        scoreboard.append({"Name": name, "Age": age, "Score": score})
+        scoreboard.sort(key=lambda x: int(x["Score"]), reverse=True)
+        scoreboard = scoreboard[:5]
+
+        # Display the scoreboard
+        print("\nTop 5 Scores:")
+        for i, row in enumerate(scoreboard[:5]):
+            print(f"{i+1}. {row['Name']} ({row['Age']}): {row['Score']}")
+
+        # Write the scoreboard data to the Google Sheet
+        sheet.clear()
+        sheet.append_row(header_row)
+        for row in scoreboard:
+            sheet.append_row([row["Name"], row["Age"], row["Score"]])
+
+    else:
+        print("Invalid input. Please enter a number from 1 to 3.")
+
+    # Ask the player if they want to play again
+    while True:
+        play_again = input("Do you want to play again? (Y/N) ")
+        if play_again.lower() == "y":
+            break
+        elif play_again.lower() == "n":
+            print("Thanks for playing!")
+            quit()
+        else:
+            print("Invalid input. Please enter Y or N.")
